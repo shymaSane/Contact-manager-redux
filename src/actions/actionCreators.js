bbdaf7b2d38a539feb3f1590301d53b3c3748a63
shymaSane <shymaSane1@gmail.com>
2018-12-05 15:08:34 +0200

    delete request to json api

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -23,9 +23,10 @@ export const addContact = (contact) => async dispatch =>{
     })
 }
 
-export const deleteContact = (id) =>{
-    return {
+export const deleteContact = (id) => async dispatch =>{
+    const res = await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+    dispatch({
         type: DELETE_CONTACT,
         payload: id
-    }
+    })
 }
